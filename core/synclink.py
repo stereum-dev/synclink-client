430b2e24c279f08372cae4734a2006cdd50b6fea
Ali Montajebi <montajebi.ali@gmail.com>
2022-10-19 09:22:32 +0330

    ADD: async filter loop

--- core/synclink.py
+++ core/synclink.py
@@ -1,29 +1,35 @@
 
+import asyncio
 from typing import List
 
 from utils.decide_majority_checkpoint import decide_majority_checkpoint
 
 from core.node import Node
+from utils.list import async_filter
 
 
 class SynclinkClient():
     def __init__(self, nodes: List[str]) -> None:
-        self.nodes = map(lambda n: Node(n), nodes)
+        self.nodes = [Node(n) for n in set(nodes)]
         self.ready_nodes: List[Node] = []
 
-    async def select_ready_nodes(self) -> List[str]:
-        for node in self.nodes:
+    async def select_ready_nodes(self) -> List[Node]:
+        async def check_is_ready(node: Node):
             health = await node.is_working()
 
             if (not health):
-                break
+                return False
 
             syncing = await node.is_syncing()
 
             if (syncing):
-                break
+                return False
 
-            self.ready_nodes.append(node)
+            return True
+
+        ready_nodes = [i async for i in async_filter(check_is_ready, self.nodes)]
+
+        self.ready_nodes = ready_nodes.copy()
 
     async def start(self):
         await self.select_ready_nodes()
