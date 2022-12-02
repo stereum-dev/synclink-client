da265cbcd7f216e1cf66a6879cb67aee06a9e26d
Ali Montajebi <montajebi.ali@gmail.com>
2022-12-02 15:14:43 +0330

    ADD: list test

---
+++ utils/tests/list_test.py
@@ -0,0 +1,24 @@
+import pytest
+import asyncio
+from utils.list import async_filter
+
+
+@pytest.mark.asyncio
+async def test_async_list_filter():
+    original_list = [1, 2, 3]
+    without_one_list = []
+
+    for item in original_list:
+        is_one = await not_one(item)
+        if is_one:
+            without_one_list.append(item)
+
+    async def not_one(item):
+        if item == 1:
+            await asyncio.sleep(1)
+            return False
+        return True
+
+    without_one_list_async = [i async for i in async_filter(not_one, original_list)]
+
+    assert without_one_list_async == without_one_list
