79a2be92abeb150cc9fe6fc8efdc769bbfc9d079
Ali Montajebi <montajebi.ali@gmail.com>
2022-12-09 12:47:20 +0330

    FIX: test error

--- utils/tests/list_test.py
+++ utils/tests/list_test.py
@@ -8,16 +8,17 @@ async def test_async_list_filter():
     original_list = [1, 2, 3]
     without_one_list = []
 
-    for item in original_list:
-        is_one = await not_one(item)
-        if is_one:
-            without_one_list.append(item)
-
     async def not_one(item):
         if item == 1:
             await asyncio.sleep(1)
             return False
+
         return True
+
+    for item in original_list:
+        is_one = await not_one(item)
+        if is_one:
+            without_one_list.append(item)
 
     without_one_list_async = [i async for i in async_filter(not_one, original_list)]
 
