7b3b94355f132bd34a77591b08166f9a71de8930
daverolo <107847185+daverolo@users.noreply.github.com>
2022-12-10 23:11:13 +0100

    TEST: improve conftest for dev settings

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
 import requests
 import pytest
+import configparser
 
 # Configure pytest
 def pytest_configure(config):
-    pytest.api_url = 'http://localhost:8000'
+    pytest.api_url = get_conftest_setting('api_url','http://localhost:8000')
     pytest.api_unavailable = api_unavailable()
     # Uncomment the below to stop ALL tests if the SyncLink Server is down
     # if pytest.api_unavailable:
@@ -21,3 +22,23 @@ def api_unavailable(api_url = None):
     except requests.exceptions.ConnectionError:
         api_unavailable.status = True
     return api_unavailable.status
+
+# Get conftest setting if either conftest.dev.ini or conftest.ini exists
+# Note that the keys in "conftest.dev.ini" will overwrite "conftest.ini"
+# The mentioned order above is mandatory and the file format is fixed:
+# [conftest]
+# key = value 
+def get_conftest_setting(key,default=None):
+    bn = "conftest"
+    ext = "ini"
+    dev = "dev"
+    filename = f"{bn}.{ext}"
+    filename_dev = f"{bn}.{dev}.{ext}"
+    config = configparser.ConfigParser()
+    try:
+        config.read([filename,filename_dev])
+    except:
+        pass
+    if not "conftest" in config:
+        return default
+    return config["conftest"][key] if key in config["conftest"] else default
